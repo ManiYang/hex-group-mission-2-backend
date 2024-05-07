0b1a09182001091070eccc8f52566b6349b207f8
AlbertYang <33620464+ManiYang@users.noreply.github.com>
2024-05-07 21:35:25 +0800

    CORS 設置

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -27,8 +28,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//
 module.exports = app;

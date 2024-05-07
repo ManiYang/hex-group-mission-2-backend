dd1effad54c133bc43a8a259d38c7fecc716eb05
AlbertYang <33620464+ManiYang@users.noreply.github.com>
2024-05-07 21:32:54 +0800

    連接資料庫

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # 第3~4堂小組任務 後端
 
+
+
+
+### .env 範例
+
+- 範例一
+  ```
+  DATABASE=mongodb://127.0.0.1:27017/db
+  ```
+- 範例二
+  ```
+  DATABASE=mongodb://username:<password>@host:port/db?options...
+  DATABASE_PASSWORD=my_password
+  ```
+  
---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// MongoDB
+const connectString = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD ??= ''
+);
+mongoose.connect(connectString).then(() => {
+    console.log('成功連上資料庫');
+})
+
+//
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;

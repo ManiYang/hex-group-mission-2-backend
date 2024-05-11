ee1a86db1ac05cf60fb4bbbbcbdc53741ccecf58
AlbertYang <33620464+ManiYang@users.noreply.github.com>
2024-05-11 20:42:00 +0800

    完成 model/posts.js

---
+++ model/posts.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: [true, '發文者名稱 userName 未提供']
+        },
+        userPhoto: {
+            type: String,
+            required: [true, '發文者照片 userPhtot 未提供']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        content: {
+            type: String,
+            required: [true, '發文內容 content 未提供']
+        },
+        image: {
+            type: String,
+            default: ''
+        }
+    },
+    // schema options:
+    {
+        versionKey: false
+    }
+)
+
+const postModel = mongoose.model('Post', postSchema);
+
+module.exports = postModel;
